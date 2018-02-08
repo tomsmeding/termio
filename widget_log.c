1835b24c4b546e2eeec9c0f7a4af1604a775ad29
Tom Smeding <tom.smeding@gmail.com>
2018-02-08 16:57:41 +0100

    GNU_SOURCE

--- widget_log.c
+++ widget_log.c
@@ -1,4 +1,4 @@
-#define _GNU_SOURCE  // vasprintf
+#define _GNU_SOURCE
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdarg.h>
--- widget_menu.c
+++ widget_menu.c
@@ -1,4 +1,3 @@
-#define _GNU_SOURCE  // vasprintf
 #include <stdio.h>
 #include <stdlib.h>
 #include <assert.h>
